3298ad4af193603915f1f63638bc3f3e4b6ce632
SUMEG-04 <sumeg04112001@gmail.com>
2025-07-18 14:58:16 +0530

    reinititaing pipeline

--- app/index.js
+++ app/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//
 
 app.get('/', (_req, res) => {
   res.send('Hello from app-code-<rollno>!');
